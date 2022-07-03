fe4675a76888c5fed39e2c5466fc3d506c2e4aa5
BANSHEE\yuris <yuris@comm-it.com>
2022-07-03 09:02:46 +0300

    Pay test

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Payeer-Payments
 Payeer-Payment-API
+
+# Create Test API for Payeer.
+Important Fields:
+1. Merchant ID
+2. Secret Key
+3. Domain
+
+# Get Answers From Payeer Suppport Team about:
+1. Verified Domains
+2. Mastercard/Visa Payments
+3. Returns Codes From Payeer
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// 👇️ "/home/john/Desktop/javascript"
+console.log("directory-name 👉️", __dirname);
+
+app.use(express.static(__dirname + "/assets"));
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
